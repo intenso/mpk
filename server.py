ee6ca3960fb42fa791420f56685b36ea8cdc0142
Adam Palmblad <adam.palmblad@gmail.com>
2011-05-17 17:22:00 +0200

    [server.py] Logging, done right.

--- server.py
+++ server.py
@@ -6,6 +6,22 @@ import subprocess
 import logging
 import SocketServer
 
+logger = logging.getLogger('mpk')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('spam.log')
+fh.setLevel(logging.INFO)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
 def lockup(host, port):
     """Allow port access from defined host """
     rule = ("/sbin/iptables",
@@ -19,42 +35,11 @@ def lockup(host, port):
     )
     subprocess.Popen(rule)
 
-def log(host, data):
-
-    logger = logging.getLogger('mpk')
-    logger.setLevel(logging.INFO)
-
-    # create file handler
-    fh = logging.FileHandler(config.LOGFILE)
-    fh.setLevel(logging.INFO)
-
-    # create console handler with a higher log level
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-
-    # create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s %(message)s %(host)s %(data)s')
-    ch.setFormatter(formatter)
-    fh.setFormatter(formatter)
-
-    # add the handlers to logger
-    logger.addHandler(ch)
-    logger.addHandler(fh)
-
-    # shout out the message
-    d = { 'host' : host, 'data' : data }
-    logger.info('Port knocked by: ', extra=d)
-
-    # remove handlers until needed again
-    logger.removeHandler(ch)
-    logger.removeHandler(fh)
-
-
 class UDPHandler(SocketServer.BaseRequestHandler):
 
     def handle(self):
         data = self.request[0].strip()
-        log(self.client_address[0], data)
+        logger.info("Port %s knocked by %s" % (data, self.client_address[0]))
         if data == "ssh":
             lockup(self.client_address[0], 22)
             
